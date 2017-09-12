5d64b78e2c7851f08afcf97564c031f895bde956
Scott <sg26laba@siena.edu>
2017-09-12 00:34:07 +0000

    added html elements and main.js file

---
+++ main.js
@@ -0,0 +1,8 @@
+function nextEquation() {
+    var element = document.getElementById("top")
+    element.innerHTML = "6"
+    element = document.getElementById("bottom")
+    element.innerHTML = "8"
+    [1,2,3,5,6,5,5,4]
+    // alert(element);
+}
