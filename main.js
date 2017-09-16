8bbd3ade13c32f1b0ffcbf5f8d44e8f7dca67895
Scott <sg26laba@siena.edu>
2017-09-16 01:14:47 +0000

    refactored game logic

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+var index = 0
+
+
 function nextEquation() {
+    
+    var topNumber = topRowNumbers()[index]
+    
     var element = document.getElementById("top")
-    element.innerHTML = "6"
-    element = document.getElementById("bottom")
-    element.innerHTML = "8"
-    [1,2,3,5,6,5,5,4]
-    // alert(element);
+    
+    element.innerHTML = topNumber
+    
+    var bottomNumber = bottomRowNumbers()[index]
+    
+    var anotherElement = document.getElementById("bottom")
+    
+    anotherElement.innerHTML = bottomNumber
+    
+    index = index + 1
+}
+
+function topRowNumbers() {
+   return [2,3,4,5,6,7,8,9];
+}
+
+function bottomRowNumbers() {
+   return [6,7,5,4,8,1,9,2];
 }
